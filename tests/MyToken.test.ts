607662bd167023b6407acfa4cd52ef84515d681f
renan04lima <renan04lima@outlook.com>
2022-08-14 20:03:10 -0300

    test: ensure only the balance of the first account must be 100 millions

--- tests/MyToken.test.ts
+++ tests/MyToken.test.ts
@@ -28,4 +28,12 @@ describe('MyToken', () => {
   it('deploys a contract', () => {
     assert.ok(myToken.options.address)
   })
+
+  it('only the balance of the first account must be 100 millions', async () => {
+    const balance = await myToken.methods.balanceOf(accounts[0]).call()
+    assert.equal(balance, 100000000 * 10 ** 10)
+
+    const balance2 = await myToken.methods.balanceOf(accounts[1]).call()
+    assert.equal(balance2, 0)
+  })
 })
